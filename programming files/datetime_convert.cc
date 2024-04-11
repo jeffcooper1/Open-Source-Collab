4ab40d5f34710d7141bd5ef4269de7d4701ee441
edwardgimlinschool <143291171+edwardgimlinschool@users.noreply.github.com>
2024-04-11 17:03:20 -0400

    Update datetime_convert.cc
    Broken

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -7,13 +7,13 @@ int main()
 {
     std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
-    std::istringstream ss(date_str);
-    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    std::istringstream ss(date_obj);
+    ss >> std::get_time(&date_str, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+    formatted_date_ss << std::put_time(&date_str, "%m/%d/%Y %M:%S:%H");
     std::string formatted_date = formatted_date_ss.str();
 
-    std::cout << formatted_date << std::endl;
+    std::cout << date_obj << std::endl;
 
     return 0;
 }
