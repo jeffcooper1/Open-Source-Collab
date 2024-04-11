09e166b642ec9239b443d7329788521844542902
swhitcomb23 <112648007+swhitcomb23@users.noreply.github.com>
2024-04-11 16:48:11 -0400

    Update sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,5 +1,8 @@
+// Makes a class called sum_even_fibonacci
 public class sum_even_fibonacci {
+    // Main function of the class
     public static void main(String[] args) {
+        // Does the fibonacci sequence several several times
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
@@ -10,6 +13,7 @@ public class sum_even_fibonacci {
             b = a + b;
             a = temp;
         }
+        // Returns the sumEven variable
         System.out.println(sumEven);
     }
 }
