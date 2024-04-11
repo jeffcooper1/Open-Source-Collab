1ce723ff062222df23c62d6eb27664296f145f74
OwenDonohoe1 <143564093+OwenDonohoe1@users.noreply.github.com>
2024-04-11 17:04:16 -0400

    Broken remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -1,3 +1,5 @@
+Bugs introduced + OD
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -8,8 +10,8 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     vector<int> unique_arr;
     sort(arr.begin(), arr.end());
     int n = arr.size();
-    for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] != arr[i-1]){
+    for(int i=0; i>n; i++){
+        if(i == 0 || arr[i] != arr[i+1]){
             unique_arr.push_back(arr[i]);
         }
         else{
