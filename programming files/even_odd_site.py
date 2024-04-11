86509cccdc91513f61074f12cdb2846f109b7ffe
83n70 <ben@pbfoot.com>
2024-04-11 17:01:47 -0400

    Broken Even_odd_site_py
    changed 2 to 3

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -4,7 +4,7 @@ with open("numbers.html", "w") as f:
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
         #Checks to see if there is remainder after calculating mod 2 on the uknown number "i" if return 0 the number is even
-        if i % 2 == 0:
+        if i % 3 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         #Else the number is odd
         else:
