5e57a56523289fee1e84c47107929a0086f42d35
OwenDonohoe1 <143564093+OwenDonohoe1@users.noreply.github.com>
2024-04-11 16:49:31 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,9 +1,12 @@
+#Checks whether or not a number is even or odd
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
+        #Checks to see if there is remainder after calculating mod 2 on the uknown number "i" if return 0 the number is even
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+        #Else the number is odd
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
